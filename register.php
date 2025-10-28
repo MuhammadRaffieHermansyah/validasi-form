239a2bf3328966c0b77da075397ec21f916089fe
sanszsx <hasanmuh497@gmail.com>
2025-10-28 16:35:38 +0700

    add label

--- register.php
+++ register.php
@@ -27,44 +27,51 @@
 <body>
     <div class="container shadow-2xl mx-auto w-[30%] rounded-xl mt-10 p-6">
         <form method="POST" action="validasi-fungsi.php">
+            <h2 class="text-2xl font-bold mb-4 text-center text-slate-700">Sign Up</h2>
             <div class="group-input grid grid-cols-1 gap-3 my-2">
                 <div class="w-full">
-                    <label class="ml-1 mb-1 block text-gray-700 font-medium after:content-['*'] after:text-red-500 after:ml-1">
+                    <label
+                        class="ml-1 mb-1 block text-gray-700 font-medium after:content-['*'] after:text-red-500 after:ml-1">
                         Nama Lengkap
                     </label>
                     <input name="nama"
                         class="w-full border border-slate-200 rounded-md px-3 py-2 focus:outline-none focus:border-slate-400" />
                 </div>
                 <div class="w-full">
-                    <label class="ml-1 mb-1 block text-gray-700 font-medium after:content-['*'] after:text-red-500 after:ml-1">
+                    <label
+                        class="ml-1 mb-1 block text-gray-700 font-medium after:content-['*'] after:text-red-500 after:ml-1">
                         Email
                     </label>
                     <input name="email"
                         class="w-full border border-slate-200 rounded-md px-3 py-2 focus:outline-none focus:border-slate-400" />
                 </div>
                 <div class="w-full">
-                    <label class="ml-1 mb-1 block text-gray-700 font-medium after:content-['*'] after:text-red-500 after:ml-1">
+                    <label
+                        class="ml-1 mb-1 block text-gray-700 font-medium after:content-['*'] after:text-red-500 after:ml-1">
                         Password
                     </label>
-                    <input name="password"type="password"
+                    <input name="password" type="password"
                         class="w-full border border-slate-200 rounded-md px-3 py-2 focus:outline-none focus:border-slate-400" />
                 </div>
                 <div class="w-full">
-                    <label class="ml-1 mb-1 block text-gray-700 font-medium after:content-['*'] after:text-red-500 after:ml-1">
+                    <label
+                        class="ml-1 mb-1 block text-gray-700 font-medium after:content-['*'] after:text-red-500 after:ml-1">
                         Konfirmasi Password
                     </label>
                     <input name="confirm_password" type="password"
                         class="w-full border border-slate-200 rounded-md px-3 py-2 focus:outline-none focus:border-slate-400" />
                 </div>
                 <div class="w-full">
-                    <label class="ml-1 mb-1 block text-gray-700 font-medium after:content-['*'] after:text-red-500 after:ml-1">
+                    <label
+                        class="ml-1 mb-1 block text-gray-700 font-medium after:content-['*'] after:text-red-500 after:ml-1">
                         Nomor Telepon
                     </label>
-                    <input name="telepon" 
+                    <input name="telepon"
                         class="w-full border border-slate-200 rounded-md px-3 py-2 focus:outline-none focus:border-slate-400" />
                 </div>
                 <div class="w-full">
-                    <label class="ml-1 mb-1 block text-gray-700 font-medium after:content-['*'] after:text-red-500 after:ml-1">
+                    <label
+                        class="ml-1 mb-1 block text-gray-700 font-medium after:content-['*'] after:text-red-500 after:ml-1">
                         Tanggal Lahir
                     </label>
                     <input name="tanggal_lahir" type="date"
